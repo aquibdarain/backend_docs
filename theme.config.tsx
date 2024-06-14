c07d0d596bcbfe36e0f2e1f1dc337ec346ade406
virtualglobetechnology1 <support@virtualglobetechnology.com>
2024-06-14 12:33:22 +0530

    creating a base documentation of the app

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  
+  logo: <span>Dozen Diamonds Backend Docs</span>,
+  
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+  
+}
+
+export default config
